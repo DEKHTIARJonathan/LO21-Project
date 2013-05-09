cc19462a717beb6e682c7eb5a84302e30d2a6bfe
Jonathan Dekhtiar <marco.flint31@gmail.com>
2013-05-09 17:51:20 +0200

    working way
    Signed-off-by: Jonathan Dekhtiar <marco.flint31@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 
 int main()
 {
-<<<<<<< HEAD
+
 	std::cout<<"ok connard"<<std::endl;
 	std::cout<<"merci connard"<<std::endl;
-=======
-    std::cout<<"ok connard\n\n";
->>>>>>> Test COmmit
+
+
     return 0;
 }
