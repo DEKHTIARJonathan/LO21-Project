a90ae4b6697ac269f262e656116bf2413127b7fc
Baune Florian <frozarmony@gmail.com>
2013-06-05 22:49:42 +0200

    Revert "fail commit"
    This reverts commit 2f6f329fddbd24e1d70a16c2e299eeb953c81602.

--- singleton.h
+++ singleton.h
@@ -1,28 +1,9 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
-template<class SingletonName>
+template<class className>
 class Singleton{
 
-	public:
-		static SingletonName&					getInstance(){
-			if( s_inst == NULL )
-				s_inst = new SingletonName();
-			return (*s_inst);
-		}
-
-		static void								destroy(){
-			if( s_inst != NULL )
-				delete s_inst;
-		}
-
-	private:
-		// Singleton
-		Singleton(){}														// Forbid direct creation
-		Singleton(const Singleton& nm);										// Forbid copy
-		Singleton&							operator=(const Singleton& n);	// Forbid copy
-		virtual ~Singleton(){}												// Forbid direct destruction
-		static SingletonName *					s_inst;						// Singleton timekeeper
 }
 
 #endif // SINGLETON_H
--- view/notesview/generalviewfactory.h
+++ view/notesview/generalviewfactory.h
@@ -4,20 +4,23 @@
 #include <QHash>
 #include <QMap>
 
-#include <singleton.h>
 #include <view/notesview/viewfactory.h>
 #include <view/notesview/viewfactorynotfoundexception.h>
 #include <note/classdef.h>
 
 #include <view/notesview/articleview.h>
 
-class GeneralViewFactory : Singleton<GeneralViewFactory>
+class GeneralViewFactory
 {
 	public:
 		// Method
 		QList<QString>							getAvailableViewFactoryType();
 		NoteView&								getView(Note& n);
 		void                                    deleteView( unsigned int id );
+
+		// Singleton
+		static GeneralViewFactory&				getInstance();
+		static void								destroy();
 
 	private:
 		// Member
@@ -29,6 +32,12 @@ class GeneralViewFactory : Singleton<GeneralViewFactory>
 
 		// Destructor
 		~GeneralViewFactory();
+
+		// Singleton
+		GeneralViewFactory();	// Interdit l'instanciation directe
+		GeneralViewFactory(const GeneralViewFactory& nm);	// Interdit la recopie
+		GeneralViewFactory&						operator=(const GeneralViewFactory& n);	// Interdit la recopie
+		static GeneralViewFactory * s_inst;	// Contient le singleton s'il est instancié
 };
 
 #endif // GENERALVIEWFACTORY_H
